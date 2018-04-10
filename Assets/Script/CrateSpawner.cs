6bb28cd8a4bdb9d619e0ce08031d88595a40b843
Thuycam Nguyen <treecam42@gmail.com>
2018-04-10 16:52:41 -0700

    Add crate spawner script

---
+++ Assets/Script/CrateSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+/**
+ * Script to instantiate a random number of crates at random positions within the map dimensions
+ * such that each crate is at least crateMinPadding tiles away from each other and
+ * the edges of the maps. 
+ * 
+ * A crate's bounds are determined by its Renderer component.
+ * A crate's position is not necessarily an integer number.
+ * @author Miv
+ */
+public class CrateSpawner
+{
+    public GameObject crate;
+    public int mapWidthInTiles;
+    public int mapHeightInTiles;
+    public int minCrates;
+    public int maxCrates;
+    // Min distance between each crates' edges
+    public float crateMinPadding;
+
+    void Start()
+    {
+        GameObject templateCrate = Instantiate(crate, Vector2.zero, Quaternion.identity);
+        Vector2 crateSize = templateCrate.GetComponent<Renderer>().bounds.size;
+
+        float minX = crateMinPadding;
+        float minY = crateMinPadding;
+        float maxX = mapWidthInTiles - maxX;
+        float maxY = mapHeightInTiles - maxY;
+
+        for (int i = 0; i < Random.Range(minCrates, maxCrates); i++)
+        {
+            Vector2 pos = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+            Instantiate(crate, pos, Quaternion.identity);
+        }
+    }
+}
